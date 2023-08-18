0a4db7597ea332ccd022d5c93175831d1dbce4b9
MHeinC <98407467+MHeinC@users.noreply.github.com>
2023-08-18 16:24:57 +0200

    feat: delete button

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,42 @@
 import { useState } from 'react'
+import { v4 as uuidv4 } from 'uuid'
 
 function App() {
   const [inputData, setInputData] = useState("")
   const [toDoArray, setToDoArray] = useState([])
 
   function handleChange(e) {
-    setInputData(prevData => {
+    setInputData(() => {
       return e.target.value
-  })}
+    }
+  )}
 
   function handleClick(e) {
     e.preventDefault()
-    if(!inputData === "") {
+    if(inputData.length > 0) {
       setToDoArray(prevState => {
-        return [...prevState, inputData]
+       return [...prevState, {
+        id: uuidv4(),
+        text: inputData
+       }]
       })
-   }
+      setInputData(() => "")
+    }
+  }
+
+  function handleDelete(e) {
+    const clickedToDo = e.target.id
+    console.log(clickedToDo)
+
+    const filterArray = toDoArray.filter(item => {
+      if(item.id !== clickedToDo) {
+        return true
+      }
+      return false
+    })
+
+    setToDoArray(filterArray)
+
   }
 
   return (
@@ -29,16 +50,23 @@ function App() {
           placeholder="Add new To Do"
           onChange={handleChange}
         />
-        <button onClick={handleClick}>Add</button>
+        <button onClick={handleClick}><i className="fa-solid fa-plus"></i></button>
       </form>
       {
         toDoArray.map(item => {
           return (
-            <div>
-              <input
-                type="checkbox"
-              />
-              <div>{item}</div>
+            <div key={uuidv4()} className="todo-container">
+              <div className="todo">
+                  <input
+                    key={uuidv4()}
+                    type="checkbox"
+                  />
+                  {item.text}
+              </div>
+              <div className="function-icons">
+                <i className="fa-regular fa-pen-to-square"></i>
+                <i className="fa-solid fa-trash-can" onClick={handleDelete} id={item.id}></i>
+              </div>
             </div>
         )})
       }
